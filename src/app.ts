fc04c1af23150ed4581d203e32decf048515d243
ariyan-hashstack <ariyan_hashstack@hashstack.finance>
2026-06-18 22:20:13 +0530

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Express } from "express"
+
+export const app: Express = express()
+
+app.get("/health",(_req, res) => {
+    res.json({
+        status: "ok",
+        timestamp: new Date().toISOString()
+    })
+})
